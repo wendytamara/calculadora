d245ed14c6d80c723dae9af4986a3ca7c6167b8d
wendytamara <anacarlavegam@gmail.com>
2018-03-10 17:43:51 -0500

    añadiendo archivos base

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+<div class="row">
+
+
+  <div class="col-md-3"></div>
+
+<div id="Calculadora" class="col-md-6">
+ <div class="panel panel-primary">
+<div class="panel-heading">Calculadora</div>
+<div class="panel-body">
+    <div class="col-md-12">
+      <div id="idCalculadora"></div>
+     </div>
+  </div>
+ </div>
+</div>
+<div class="col-md-3"></div>
+</div>
+</div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
